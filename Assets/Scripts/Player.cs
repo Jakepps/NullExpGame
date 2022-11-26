8052acae453ccee836857cf0e546567d8dd76e76
StarProxima <maks.prozorov1@yandex.ru>
2022-11-26 18:41:15 +0300

    Revert "Anim getDamage"
    This reverts commit 58efa2d96eb3bb9dfcbc33eb3833e96a8e3f2003.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    float health = 100;
-
-    float attackColdDown = 0.5f;
+    int health = 100;
 
     // Attack others players by radius 10
     void Attack()
@@ -22,28 +20,23 @@ public class Player : MonoBehaviour
         //     transform.position + new Vector3((flipX ? -1 : 1) * 1.5f, 1, 0),
         //     Quaternion.identity
         // );
-        if (attackColdDown < 0.1)
-        {
-            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position + new Vector3((flipX ? -1 : 1) * 1.75f, 0, 0), 1f);
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position + new Vector3((flipX ? -1 : 1) * 1.5f, 0, 0), 1.5f);
 
-            Debug.Log(hitColliders.Length);
-            foreach (var hitCollider in hitColliders)
+        Debug.Log(hitColliders.Length);
+        foreach (var hitCollider in hitColliders)
+        {
+            // if enemy is found
+            if (hitCollider.gameObject.tag == "Enemy")
             {
-                // if enemy is found
-                if (hitCollider.gameObject.tag == "Enemy")
-                {
-                    attackColdDown = 0.5f;
-                    Debug.Log("Enemy found");
-                    // get enemy script
-                    Enemy enemy = hitCollider.gameObject.GetComponent<Enemy>();
-                    // attack enemy
-                    gameObject.GetComponentInChildren<Rigidbody2D>().velocity = Vector2.zero;
-                    enemy.TakeDamage(10);
-                    enemy.TakeImpulse(new Vector2((flipX ? -1 : 1) * 1000f, 0));
-                }
+
+                Debug.Log("Enemy found");
+                // get enemy script
+                Enemy enemy = hitCollider.gameObject.GetComponent<Enemy>();
+                // attack enemy
+                enemy.TakeDamage(10);
+                enemy.TakeImpulse(new Vector2((flipX ? -1 : 1) * 1000f, 0));
             }
         }
-
 
 
 
@@ -52,7 +45,7 @@ public class Player : MonoBehaviour
     //take damage
     public void TakeDamage(int damage)
     {
-        gameObject.GetComponentInChildren<Animator>().Play("getDamage", 0, 0.2f);
+        gameObject.GetComponentInChildren<Animator>().Play("getDamage", 0, 0.5f);
         health -= damage;
         if (health <= 0)
         {
@@ -84,13 +77,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (attackColdDown > 0)
-        {
-            attackColdDown -= Time.deltaTime;
-        }
-
-        health += Time.deltaTime;
-
         // Attack on press left mouse button
         if (Input.GetMouseButtonDown(0))
         {
